192c5d01688ce4e65bcaeefdada2e145ea428f54
MillerMora <millermora2022@gmail.com>
2025-11-21 11:32:35 -0500

    hola

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api/recetas', (req, res) => {
+  res.json('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(3000, () => {
+  console.log("arranco el servidor")
 })
