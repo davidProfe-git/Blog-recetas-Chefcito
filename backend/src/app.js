a5a7ecb2887d133c998617e151a7c7a2a32d98d5
david rugeles <test@test.com>
2025-11-27 12:37:43 -0500

    ultima version viernes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 4000
 
-let estudiante = ["laura","miguel"]
-
-app.get('/prueba', (req, res) => {
-    res.json({mensaje: "hola clase prog 1"})
+app.get('/api/recetas',(request,reponse)=>{
+  reponse.json({mensaje : "funciona mi api"})
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(3000, ()=>{
+  console.log("arranco el servidor")
 })
