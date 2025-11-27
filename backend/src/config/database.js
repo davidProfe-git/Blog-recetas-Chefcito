205b27a1d986e66ae5f719853c8c3077404dbd7c
david rugeles <test@test.com>
2025-11-27 13:11:37 -0500

    inicio conexion con base de datos

---
+++ backend/src/config/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2')
+
+
+const coneccion = mysql.createPool({
+    host : localhost,//donde esta la base de datos
+    user: root,//usuario de base de datos
+    passoword: 1234 //password de la base de datos
+
+})
+
