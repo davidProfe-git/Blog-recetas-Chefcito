d3c48062170d50f8720ee3f165845f0808ff0b79
prueba <prueba@prueba.com>
2025-12-05 13:16:40 -0500

    queda casi listo el sitio usenlo de ejemplo

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const cors = require('cors')
 const PORT = process.env.PORT
 const router = require('./routes/recetasRouter')
 
+app.use(cors())
 app.use('/api/recetas',router)
 
 
