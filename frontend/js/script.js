38c349a50c602545dee9f721af8e577da6513667
estebanbernalmonroyedc-afk <estebanbernalmonroyedc@gmail.com>
2025-12-11 12:30:33 -0500

    hola

--- frontend/js/script.js
+++
@@ -1,7 +0,0 @@
-alert("vamos a jugar piedra papel o tijera")
-let jugador = prompt("¿cual eliges? piedra papel o tijeras")
-let pc = Math.floor(Math.random()* (3 - 1 + 1) + 1)
-if (pc === 1) eleccionPc = "piedra";
-else if (pc === 2) eleccionPc = "papel";
-else eleccionPc = "tijera";
-
