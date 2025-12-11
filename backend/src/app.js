a95bcdb520c0c75beca3dd4e5c9178583a94052d
estebanbernalmonroyedc-afk <estebanbernalmonroyedc@gmail.com>
2025-12-11 12:49:36 -0500

    se modifican app y env

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const PORT = process.env.PORT
+const cors = require('cors')
 const router = require('./routes/recetasRouter')
 
+
+app.use(cors())
 app.use('/api/recetas',router)
 
 
